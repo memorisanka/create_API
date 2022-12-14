038f186c29c557bd3c24b648f98359eebf202357
memorisanka <j.krawczyk87@gmail.com>
2022-12-14 15:43:01 +0100

    feat(snippets): add user authentication

--- snippets/models.py
+++ snippets/models.py
@@ -19,7 +19,9 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
-    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
+    )
     highlighted = models.TextField()
 
     class Meta:
@@ -31,9 +33,10 @@ class Snippet(models.Model):
         representation of the code snippet.
         """
         lexer = get_lexer_by_name(self.language)
-        linenos = 'table' if self.linenos else False
-        options = {'title': self.title} if self.title else {}
-        formatter = HtmlFormatter(style=self.style, linenos=linenos,
-                                  full=True, **options)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
         self.highlighted = highlight(self.code, lexer, formatter)
         super().save(*args, **kwargs)
